9414e318bfd8d89b872008ae593c2ce6e011cc70
Eusebiu <Eusebiu@totalreality.nl>
2021-05-08 01:05:55 +0200

    added shop

---
+++ Assets/ShopButtonScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButtonScript : MonoBehaviour
+{
+    public bool bullet, aoe, wall;
+    private ShootingScript shootingScript;
+    private ShopScript shopScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopScript = FindObjectOfType<ShopScript>();
+        shootingScript = FindObjectOfType<ShootingScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void IncrementLevel()
+    {
+        if (shopScript.skillPointsLeft > 0)
+        {
+            if (bullet)
+            {
+                shootingScript.IncrementBullet();
+            }
+            if (aoe)
+            {
+                shootingScript.IncrementAOE();
+            }
+            if (wall)
+            {
+                shootingScript.IncrementLine();
+            }
+            shopScript.skillPointsLeft--;
+        }
+    }
+}
