5dac1ecad02975aca724b4b10132bdd812cbba93
Eusebiu <Eusebiu@totalreality.nl>
2021-05-07 20:51:29 +0200

    Added scene changer and new projectiles

---
+++ Assets/CollisionEventTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CollisionEventTrigger : MonoBehaviour
+{
+    public string TriggerObjectName;
+    public UnityEvent eventToTrigger;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == TriggerObjectName)
+        {
+            eventToTrigger.Invoke();
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == TriggerObjectName)
+        {
+            eventToTrigger.Invoke();
+        }
+    }
+}
--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -7,20 +7,74 @@ public class ShootingScript : MonoBehaviour
     [SerializeField]
     private GameObject Bullet;
     [SerializeField]
+    private GameObject AoEObject;
+    [SerializeField]
+    private GameObject WallObject;
+
+    [SerializeField]
     private GameObject BulletOrigin;
+
+    public enum WeaponType{ bullet,aoe,wall};
+
+    public WeaponType weaponType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        weaponType = WeaponType.bullet;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            weaponType = WeaponType.bullet;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            weaponType = WeaponType.aoe;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            weaponType = WeaponType.wall;
+        }
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         
+
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(Bullet, BulletOrigin.transform.position, transform.rotation);
+            switch (weaponType)
+            {
+                case WeaponType.bullet:
+                    Instantiate(Bullet, BulletOrigin.transform.position, transform.rotation);
+                    break;
+                case WeaponType.aoe:
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.transform.gameObject.tag == "Floor")
+                        {
+                            Instantiate(AoEObject, hit.point, Quaternion.identity);
+                        }
+                    }
+                   
+                    break;
+                case WeaponType.wall:
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.transform.gameObject.tag == "Floor")
+                        {
+                           GameObject wall= Instantiate(WallObject, hit.point, Quaternion.identity);
+                            wall.transform.rotation = gameObject.transform.rotation;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
