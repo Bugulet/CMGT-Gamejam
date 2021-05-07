58debc49c004f6b306ccaa19d772f193432072c0
Patrik <pnyilka@icloud.com>
2021-05-07 19:13:43 +0200

    VFX

---
+++ Assets/VFX/Enemy#1/Scripts/Hover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+
+    public float verticalSpeed;
+    public float horizontalSpeed;
+
+    public float amplitude;
+
+    private Vector3 tempPosition;
+    //public Vector3
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        tempPosition = this.transform.position;
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
+    }
+}
